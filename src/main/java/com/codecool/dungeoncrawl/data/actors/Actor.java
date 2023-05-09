14dc6e63374ce9eb70eef9def5d1639774d24257
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-05-09 18:18:55 +0200

    Reverting to 624e473

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
@@ -6,20 +6,17 @@ import com.codecool.dungeoncrawl.data.DayPeriod;
 import com.codecool.dungeoncrawl.data.Drawable;
 
 public abstract class Actor implements Drawable {
-
-    protected Cell cell;
+    private Cell cell;
     private DayPeriod dayPeriod;
     private int health = 10;
 
     public Actor(Cell cell, DayPeriod dayPeriod) {
-
         this.cell = cell;
         this.dayPeriod = dayPeriod;
         this.cell.setActor(this);
     }
 
-
-
+    public void move(int dx, int dy) {
 
         Cell nextCell = cell.getNeighbor(dx, dy);
         if (checkForWall(nextCell)) {
@@ -48,26 +45,24 @@ public abstract class Actor implements Drawable {
         return nextCell.getActor().getTileName().equals("friend");
     }
 
-
     private boolean checkIfIsEmpty(Cell nextCell) {
         return nextCell.getActor() == null;
     }
 
-
+    private boolean checkForWall(Cell nextCell) {
+        return !nextCell.getType().equals(CellType.WALL);
+    }
 
     private void attack(Cell nextCell) {
+        nextCell.getActor();
+    }
 
-        setHealth(getHealth()-2);
-
+    private boolean checkEnemy(Cell nextCell) {
+        return nextCell.getType().getTileName().equals("enemy");
     }
 
     public int getHealth() {
         return health;
-    }
-
-
-    public void setHealth(int health) {
-        this.health = health;
     }
 
 
--- src/main/java/com/codecool/dungeoncrawl/data/actors/Enemy.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Enemy.java
@@ -1,44 +1,18 @@
 package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
-
-import com.codecool.dungeoncrawl.data.CellType;
-
-public class Enemy extends Actor{
-
 import com.codecool.dungeoncrawl.data.DayPeriod;
 
 public class Enemy extends Actor{
     public Enemy(Cell cell, DayPeriod dayPeriod) {
         super(cell, dayPeriod);
-
     }
 
     @Override
     public String getTileName() {
         return null;
     }
-
-
-    public String getTileType(){
-
-
-    public void move(int dx, int dy) {
-
-        Cell nextCell = cell.getNeighbor(dx, dy);
-        if (checkForWall(nextCell) && checkPlayer(nextCell)) {
-            cell.setActor(null);
-            nextCell.setActor(this);
-            cell = nextCell;
-        }
-
+    public String getActorType(){
+        return "enemy";
     }
-
-    private boolean checkPlayer(Cell cell) {
-        return cell.getActor() instanceof Player;
-    }
-
-    private boolean checkForWall(Cell nextCell){
-        return !nextCell.getType().equals(CellType.WALL);
-    }
-
+}
