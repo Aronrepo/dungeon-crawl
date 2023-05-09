296bb61d6f0ba3cc8687e7868a67ff1e72e6f4e9
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-05-09 14:38:38 +0200

    connect day-night cycle to game-logic

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
@@ -1,10 +1,11 @@
 package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.DayPeriod;
 
 public class Player extends Actor {
-    public Player(Cell cell) {
-        super(cell);
+    public Player(Cell cell, DayPeriod dayPeriod) {
+        super(cell, dayPeriod);
     }
 
     public String getTileName() {
--- src/main/java/com/codecool/dungeoncrawl/data/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Skeleton.java
@@ -1,10 +1,11 @@
 package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.DayPeriod;
 
 public class Skeleton extends Actor {
-    public Skeleton(Cell cell) {
-        super(cell);
+    public Skeleton(Cell cell, DayPeriod dayPeriod) {
+        super(cell, dayPeriod);
     }
 
     @Override
