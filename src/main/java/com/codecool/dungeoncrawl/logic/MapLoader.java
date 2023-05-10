e724a053fb6d435032a14cea745fe790a6865719
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-05-10 23:44:10 +0200

    design end scenes

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -146,66 +146,19 @@ public class MapLoader {
                 if (x < line.length()) {
                     Cell cell = map.getCell(x, y);
                     switch (line.charAt(x)) {
-                        case ' ':
-                            cell.setType(CellType.EMPTY);
-                            break;
                         case '#':
                             cell.setType(CellType.WALL);
-                            break;
-                        case '.':
-                            cell.setType(CellType.FLOOR);
-                            break;
-                        case 's':
-                            cell.setType(CellType.FLOOR);
-
-                            map.setSkeleton(new Skeleton(cell));
                             break;
                         case '@':
                             cell.setType(CellType.FLOOR);
                             map.setPlayer(new Player(cell));
                             break;
-                        case 'p':
-                            cell.setType(CellType.FLOOR);
-                            new PussInBoots(cell);
-                            break;
-                        case 'd':
-                            cell.setType(CellType.FLOOR);
-                            new Donkey(cell);
-                            break;
-                        case 'D' :
-                            cell.setType(CellType.GROUND);
-                            map.setDragon(new Dragon(cell));
-                            break;
-                        case 'F' :
-                            cell.setType(CellType.FLOOR);
-                            map.setFarquad(new Farquad(cell));
-                            break;
                         case 'f' :
                             cell.setType(CellType.FLOOR);
                             map.setFiona(new Fiona(cell));
                             break;
-                        case 'l' :
-                            cell.setType(CellType.LAVA);
-                            break;
-                        case 't' :
-                            cell.setType(CellType.TREE);
-                            break;
-                        case 'c':
-                            cell.setType(CellType.CASTLE);
-                            break;
-                        case 'm':
-                            cell.setType(CellType.MUD);
-                            break;
                         case 'g':
                             cell.setType(CellType.GROUND);
-                            break;
-                        case 'k':
-                            cell.setType(CellType.FLOOR);
-                            new Sword(cell);
-                            break;
-                        case '?' :
-                            cell.setType(CellType.GROUND);
-                            new Fire(cell);
                             break;
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
